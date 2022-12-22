ac0b9f6e4ffaf02ac65f44808a928aa6245403e3
Henrique Martins <hmartins224@gmail.com>
2022-12-21 22:18:24 -0300

    integrate open ai whith whatsapp

--- index.ts
+++ index.ts
@@ -1,17 +1,51 @@
+import { Client } from "whatsapp-web.js";
 import { Configuration, OpenAIApi } from "openai";
+
+import { generate } from "qrcode-terminal";
+
+const clientWhatsApp = new Client({
+  puppeteer: {
+    args: ["--no-sandbox"],
+  },
+});
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-async function openAiTest() {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: "Create a hello world in python",
-    temperature: 0.6,
-  });
-  console.log(completion.data.choices[0].text);
+function openAIGenerateResponseByText(text: string) {
+  return openai
+    .createCompletion({
+      model: "text-davinci-003",
+      prompt: text,
+      max_tokens: 1000,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ["{}"],
+    })
+    .then((response) => {
+      return response.data.choices[0].text;
+    });
 }
 
-openAiTest();
+clientWhatsApp.on("qr", (qr) => {
+  console.log(qr);
+  generate(qr);
+});
+
+clientWhatsApp.on("ready", () => {
+  console.log("Client WhatsApp is ready!");
+});
+
+clientWhatsApp.initialize();
+
+clientWhatsApp.on("message", (message) => {
+  if (message.body[0] === "*") {
+    console.log(message.body);
+    openAIGenerateResponseByText(message.body).then((response) => {
+      message.reply(response);
+    });
+  }
+});
